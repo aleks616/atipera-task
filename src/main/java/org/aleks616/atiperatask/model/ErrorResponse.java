3bb20d4518a6a93c78eb2bb0bc8c6620da00765f
mantikora-pl <aleksjankowiak5230@gmail.com>
2025-07-29 16:24:09 +0200

    added models: repository, branch, error response

---
+++ src/main/java/org/aleks616/atiperatask/model/ErrorResponse.java
@@ -0,0 +1,9 @@
+package org.aleks616.atiperatask.model;
+
+public class ErrorResponse{
+    public int status;
+    public String message;
+
+    public ErrorResponse(int value,String message){
+    }
+}
